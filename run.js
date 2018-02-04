2d7a2ab5c47688515b74483a53149eeb2d57630a
Melissa Sue Aprea <msp13@cornell.edu>
2018-02-04 14:07:20 -0500

    consolidate sensititre data

--- run.js
+++ run.js
@@ -21,6 +21,7 @@ const unique_name_prefix = `${state}${zipcode}PPY1`;
 const combined_isolates_csv = fs.readFileSync(path.join(input_data_folder, combined_isolates_filename), 'utf8');
 const sensititre_csv = fs.readFileSync(path.join(input_data_folder, sensititre_filename), 'utf16le').replace(/[\t]+/, '\t'); // remove consecutive delimieters
 
+// pre-process combined isolates data
 let combined_isolates_data = parse(combined_isolates_csv, {columns: true})
 let starting_number = combined_isolates_data.map(r => r['Unique Specimen ID'].slice(-4))
     .filter(v => v.trim()) // get rid of blanks
@@ -30,10 +31,44 @@ if(starting_number === -Infinity){
 }
 starting_number++;
 console.log(`starting number will be ${zeroFill(4, starting_number)}`);
+combined_isolates_data = combined_isolates_data.filter(r => r.Include.toLowerCase() === 'yes');
+console.log(`${combined_isolates_data.length} accessions will be included`);
 
-combined_isolates_data.filter(r => r.Include.toLowerCase() === 'yes');
+// pre-process sensititre data
+let sensititre_data = parse(sensititre_csv, {delimiter: '\t'})
+const sensititre_violations = sensititre_data.filter(d => d.length !== 340);
+if(sensititre_violations.length > 0){
+    console.dir(sensititre_violations);
+    console.error(`Sensitre data has ${sensititre_violations.length} rows with non-compliant column length`);
+    process.exit(1);
+}
 
-const sensititre_data = parse(sensititre_csv, {delimiter: '\t'})
-console.log(combined_output_headers);
-console.log(combined_isolates_data[0])
-console.log(combined_isolates_data.map(r => r['Accession #']));
+let post_sensitire_data = sensititre_data.map(r => {
+    let date_value = r[39]; 
+    let well_data = r.slice(40);
+    let consolidated_well_data = [];
+    // there are 100 wells worth of data, 3 columns per well
+    for(let i = 0; i < 100; i++){
+        const base = i * 3;
+        const a = well_data[base].trim(), b = well_data[base+1].trim(), c = well_data[base+2].trim();
+        if(a || b || c){
+            consolidated_well_data = consolidated_well_data.concat([a,b,c]);
+        }
+
+    }
+    return [date_value].concat(consolidated_well_data);
+});
+
+console.log(post_sensitire_data[0]);
+console.log(post_sensitire_data[1]);
+
+let allOutputDataRows = combined_isolates_data.map(r => {
+    const accession_number =  r['Accession #'];
+    let row = combined_output_headers.map(h => r[h]);
+    let corresponding_sensitire_row = sensititre_data.find(s => s[6] === accession_number); // 6 is 'column G' in the sensititre data
+    if(!corresponding_sensitire_row){
+        console.error(`Can't find sensititre record for Accesssion #: '${accession_number}'`);
+        process.exit(2);
+    }
+    row = row.concat(corresponding_sensitire_row);
+})
